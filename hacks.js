1090304a27abee3b7e1a06ecfa02e973bbe2bee0
ryniee3 <120175662+ryniee3@users.noreply.github.com>
2022-12-09 04:09:00 -0600

    Update hacks.js

--- hacks.js
+++ hacks.js
@@ -226,7 +226,7 @@ javascript: (async () => {
         let i = 0;
 
         for (const [k, v] of Object.entries(hacks[game])) {
-            hacksmsg = hacksmsg + `${k} `;
+            //hacksmsg = hacksmsg + `${k} `;
             i++;
         };
 
@@ -235,14 +235,14 @@ javascript: (async () => {
         let hack = prompt(selectHackMsg + "\n" + hacksmsg);
 
         if (i === 0) return;
-        if (hacks[game][hack]) {
-            hacks[game][hack](reactHandler).catch((reason) => {
-                alert(reason.message + "\n" + reason);
+        //if (hacks[game][hack]) {
+        //    hacks[game][hack](reactHandler).catch((reason) => {
+        //        alert(reason.message + "\n" + reason);
 
-            })
-        } else {
+       //     })
+        //} else {
             alert('This game does not have a hack called "' + hack + '"');
-        }
+        //}
     } else {
         alert('You must be on play.blooket.com to use this!');
     }
